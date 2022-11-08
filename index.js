768e25d9aa5eb400c03636de1d268a66f635e2b2
Андрей <andrevyakin@gmail.com>
2022-11-08 18:44:18 +0300

    Created variables Substituted variables in the text

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const programmingLanguage = "JavaScript";
 const courseCreatorName = "Команда Result School";
 const reasonText = "Мне нечем больше заняться";
 const numberOfMonth = 0;
+
+console.log(`Всем привет! Меня зовут ${myName}.
+Сейчас я изучаю язык программирования ${programmingLanguage}
+на курсе по ${programmingLanguage} у ${courseCreatorName}.
+Я хочу стать веб-разработчиком, потому что ${reasonText}.
+До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а).
+Я уверен(а), что пройду данный курс до конца!`);
