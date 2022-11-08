08087f49a76b96ef161e5545e5d5ff44f9e5ac10
Андрей <andrevyakin@gmail.com>
2022-11-08 18:34:08 +0300

    Created variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const myName = "Андрей";
+const programmingLanguage = "JavaScript";
+const courseCreatorName = "Команда Result School";
+const reasonText = "Мне нечем больше заняться";
+const numberOfMonth = 0;
